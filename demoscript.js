2c73c096a7f118b99e9e0c706b33f903b61aedec
TheGreatMaximus98 <87099103+TheGreatMaximus98@users.noreply.github.com>
2023-10-30 16:52:14 -0400

    Create demoscript.js

---
+++ demoscript.js
@@ -0,0 +1,252 @@
+// Creator: Maximus Halloran
+// Music Taken Down for Copyright reasons.
+
+var click = document.getElementById("click");
+var clicktxt = document.getElementById("click-text");
+var clicks = 0;
+var clickAnimation = false;
+var perSec = 0;
+var music = false;
+var mIndex = 0;
+var ppc = 1;
+var rebirths = 0;
+var clickUpgrades = 0;
+var clickUpgradeCost = 10;
+var crowdUpgrades = 0;
+var crowdUpgradeCost = 100;
+var maxwellUpgrades = 0;
+var maxwellUpgradeCost = 1000;
+document.addEventListener("keydown", (e) => {
+  if (e.key == "m"){
+    e.preventDefault()
+    if (mIndex == 0){
+      mIndex = 1
+      document.querySelector("#scawyMusic").src = "//cdn.crazycontent.net/New Friendly.mp3"
+    } else {
+      mIndex = 0
+      document.querySelector("#scawyMusic").src = "//cdn.crazycontent.net/Off to Osaka.mp3"
+    }
+  }
+})
+function load(){
+  if (getCookie("cheater")){
+    window.location.href = "/cheater"
+  }
+  if (getCookie("rebirths")){
+    rebirths = parseInt(getCookie("rebirths"))
+  }
+  if (getCookie("clicks")){
+    clicks = parseInt(getCookie("clicks"))
+  }
+  if (getCookie("ppc")){
+    ppc = parseInt(getCookie("ppc"))
+    document.getElementById("ppc").innerHTML = ppc + " PPC";
+  }
+  if (getCookie("perS")){
+    perSec = parseInt(getCookie("perS"))
+    document.getElementById("per-sec").innerHTML = perSec + " PPS";
+  }
+  if (getCookie("clickU")){
+    clickUpgrades = parseInt(getCookie("clickU"))
+    document.getElementById("click-upgrade-times").innerHTML = clickUpgrades;
+  }
+  if (getCookie("clickUCost")){
+    clickUpgradeCost = parseInt(getCookie("clickUCost"))
+    document.getElementById("click-upgrade-cost").innerHTML = clickUpgradeCost;
+  }
+  if (getCookie("crowdU")){
+    crowdUpgrades = parseInt(getCookie("crowdU"))
+    document.getElementById("crowd-upgrade-times").innerHTML = crowdUpgrades;
+  }
+  if (getCookie("crowdUCost")){
+    crowdUpgradeCost = parseInt(getCookie("crowdUCost"))
+    document.getElementById("crowd-upgrade-cost").innerHTML = crowdUpgradeCost;
+  }
+  if (getCookie("maxwellU")){
+    maxwellUpgrades = parseInt(getCookie("maxwellU"))
+    document.getElementById("maxwell-upgrade-times").innerHTML = maxwellUpgrades;
+  }
+  if (getCookie("maxwellUCost")){
+    maxwellUpgradeCost = parseInt(getCookie("maxwellUCost"))
+    document.getElementById("maxwell-upgrade-cost").innerHTML = maxwellUpgradeCost;
+  }
+}
+function save(){
+  document.cookie = "rebirths="+rebirths+"; "
+  document.cookie = "clicks="+clicks+";"
+  document.cookie = "ppc="+ppc+";"
+  document.cookie = "perS="+perSec+";"
+  document.cookie = "clickU="+clickUpgrades+";"
+  document.cookie = "clickUCost="+clickUpgradeCost+";"
+  document.cookie = "crowdU="+crowdUpgrades+";"
+  document.cookie = "crowdUCost="+crowdUpgradeCost+";"
+  document.cookie = "maxwellU="+maxwellUpgrades+";"
+  document.cookie = "maxwellUCost="+maxwellUpgradeCost+";"
+}
+window.addEventListener("click", function () {
+  if (music == false){
+    music = true
+    var scary = new Audio("//cdn.crazycontent.net/Off to Osaka.mp3");
+    scary.addEventListener("canplaythrough", function () {
+      scary.loop = true;
+      document.getElementById("sfx").appendChild(scary)
+      scary.id = "scawyMusic"
+      scary.play();
+    });
+  }
+});
+window.addEventListener("onbeforeunload", save());
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function clickupgrade() {
+  if (clicks > clickUpgradeCost - 1) {
+    clickUpgrades += 1;
+    document.getElementById("click-upgrade-times").innerHTML = clickUpgrades;
+    var buysfx = new Audio(
+      "/buy.mp3"
+    );
+    buysfx.addEventListener("canplaythrough", (event) => {
+      buysfx.play();
+    });
+    clicks = clicks - clickUpgradeCost;
+    clickUpgradeCost = Math.floor(clickUpgradeCost * 1.5);
+    document.getElementById("click-upgrade-cost").innerHTML = clickUpgradeCost;
+    clicktxt.innerHTML = Math.floor(clicks) + " people";
+    ppc = ppc + 1;
+    document.getElementById("ppc").innerHTML = ppc + " PPC";
+  } else {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
+      document.getElementById("cantAfford")
+    );
+    if (clickUpgradeCost - clicks == 1) {
+      document.getElementById("alert-end").innerHTML = "more person.";
+    } else {
+      document.getElementById("alert-end").innerHTML = "more people.";
+    }
+    document.getElementById("alert-cost").innerHTML = clickUpgradeCost - clicks;
+    toastBootstrap.show();
+  }
+}
+function crowdupgrade() {
+  if (clicks > crowdUpgradeCost - 1) {
+    if (crowdUpgrades == 4){
+      document.getElementById('crowd-upgrade').disabled = true
+    }
+    crowdUpgrades += 1;
+    document.getElementById("crowd-upgrade-times").innerHTML = crowdUpgrades;
+    var buysfx = new Audio("/buy");
+    buysfx.addEventListener("canplaythrough", (event) => {
+      buysfx.play();
+    });
+    clicks = clicks - crowdUpgradeCost;
+    crowdUpgradeCost = Math.floor(crowdUpgradeCost * 1.5)
+    document.getElementById("crowd-upgrade-cost").innerHTML = crowdUpgradeCost
+    clicktxt.innerHTML = Math.floor(clicks) + " people"
+    perSec = perSec + 10
+    document.getElementById("per-sec").innerHTML = Math.floor(perSec) + " PPS"
+  } else {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
+      document.getElementById("cantAfford")
+    );
+    if (clickUpgradeCost - clicks == 1) {
+      document.getElementById("alert-end").innerHTML = "more person.";
+    } else {
+      document.getElementById("alert-end").innerHTML = "more people.";
+    }
+    document.getElementById("alert-cost").innerHTML = crowdUpgradeCost - clicks;
+    toastBootstrap.show();
+  }
+}
+function maxwellupgrade() {
+  if (clicks > maxwellUpgradeCost - 1) {
+    if (maxwellUpgrades == 4){
+      document.getElementById('maxwell-cat-upgrade').disabled = true
+    }
+    maxwellUpgrades += 1;
+    document.getElementById("maxwell-upgrade-times").innerHTML = maxwellUpgrades;
+    var buysfx = new Audio(
+      "/buy.mp3"
+    );
+    buysfx.addEventListener("canplaythrough", (event) => {
+      buysfx.play();
+    });
+    clicks = clicks - maxwellUpgradeCost;
+    maxwellUpgradeCost = Math.floor(maxwellUpgradeCost * 1.5);
+    document.getElementById("maxwell-upgrade-cost").innerHTML = maxwellUpgradeCost;
+    clicktxt.innerHTML = Math.floor(clicks) + " people";
+    if (ppc == 1){
+      ppc = 10;
+    } else {
+      if (ppc > 1){
+        ppc = ppc + 10;
+      }
+    }
+    if (maxwellUpgrades == 1){
+      document.getElementById("maxwell-hat").id = "maxwell-hat-active"
+      const maxwell = bootstrap.Toast.getOrCreateInstance(
+        document.getElementById("maxwell")
+      )
+      maxwell.show();
+    }
+    document.getElementById("ppc").innerHTML = Math.floor(ppc) + " PPC";
+  } else {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
+      document.getElementById("cantAfford")
+    );
+    if (clickUpgradeCost - clicks == 1) {
+      document.getElementById("alert-end").innerHTML = "more person.";
+    } else {
+      document.getElementById("alert-end").innerHTML = "more people.";
+    }
+    document.getElementById("alert-cost").innerHTML = maxwellUpgradeCost - clicks;
+    toastBootstrap.show();
+  }
+}
+setInterval(function () {
+  clicks += perSec;
+  clicktxt.innerHTML = Math.floor(clicks) + " people";
+}, 1000);
+function add() {
+  if (clickAnimation == true) {
+    document.getElementById("click-img").style.animation = "";
+  }
+  clicks += ppc * (rebirths+1);
+  clicktxt.innerHTML = Math.floor(clicks) + " people";
+  clickAnimation = true;
+  document.getElementById("click-img").style.animation = "click 0.2s";
+  setTimeout(function () {
+    clickAnimation = false;
+    document.getElementById("click-img").style.animation = "";
+  }, 200);
+}
+$("#click").click(function () {
+  add();
+});
+$("#click-upgrade").click(function () {
+  clickupgrade();
+});
+$("#crowd-upgrade").click(function () {
+  crowdupgrade();
+});
+$("#maxwell-cat-upgrade").click(function () {
+  maxwellupgrade();
+});
+window.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (event.code == "Space") {
+    add();
+  }
+});
+setTimeout(load, 100)
